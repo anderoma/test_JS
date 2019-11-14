751d8c3f5a0b06f68f0b028581e053b4b3576bf9
anderoma <antoine.deroma@gmail.com>
2019-11-14 22:32:33 +0100

    script5

--- script_5.js
+++ script_5.js
@@ -48,14 +48,47 @@ function less_rented(books){
   console.log(books[tmp]);
 }
 
-function shearch_id(books){
+function shearch_id(books, id){
   let i = 0;                       
   while (i < books.length){
-    if (books[i].id == 873495){
+    if (books[i].id == id){
       return (console.log(books[i]));
     }
     i++;
   }
+  return (console.log(`le livre n° ${id} n'as pas était trouvé`));
+}
+
+function supprime(books, id){
+  let i = 0;                       
+  while (i < books.length){
+    if (books[i].id == id){
+      books.splice(i,i + 1);
+    }
+    i++;
+  }
+  return (books);
+}
+function affichage(books_2){
+  j = 0;
+  while (j < books_2.length){
+    console.log(books_2[j])
+    j++;
+  }
+}
+function swap(books_2){
+  let i = 0;
+  let tmp;
+  while (i + 1 < books_2.length){
+    if (books_2[i].title > books_2[i + 1].title){
+      tmp = books_2[i];
+      books_2[i] = books_2[i + 1];
+      books_2[i + 1] = tmp;
+      i = -1;
+    }
+    i++;
+  }
+  return (books_2);
 }
 
 console.log("Est-ce que tous les livres ont été au moins empruntés une fois ?");
@@ -68,4 +101,13 @@ console.log("Quel est livre le moins emprunté ?");
 less_rented(books);
 
 console.log("le livre avec l'ID: 873495");
-shearch_id(books);
+shearch_id(books, 873495);
+
+console.log("Supprime le livre avec l'ID: 133712");
+shearch_id(books, 133712);
+let books_2 = supprime(books, 133712);
+shearch_id(books, 133712);
+
+console.log("Trie les livres par ordre alphabétique");
+books_2 = swap(books_2);
+affichage(books_2);
